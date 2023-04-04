ef1e6549b359a56e9313cbf34fb9166087fbacf0
ananya <ananyagudapati9@gmail.com>
2023-04-04 07:35:31 -0500

    commit comment

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,26 @@
+import java.util.*;
+
 public class Player {
-    //testing
+    ArrayList<String> specialTokens;
+    ArrayList<Houses> Houses;
+    private int remainingHouses;
+    private String color;
+    private int score;
+
+    public Player(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getHouseCount() {
+        return remainingHouses;
+    }
+
+    public void addSpecialToken(String token) {
+        specialTokens = new ArrayList<>();
+        specialTokens.add(token);
+    }
 }
