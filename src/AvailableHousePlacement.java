6270a83519ab8b3769b5f0b8033d6efa1a93239f
Stanley <stanleymmm2006@gmail.com>
2023-04-11 07:40:17 -0500

    commit GameLogic.java

--- src/AvailableHousePlacement.java
+++ src/AvailableHousePlacement.java
@@ -1,2 +1,13 @@
+import java.util.ArrayList;
+
 public class AvailableHousePlacement {
+    public AvailableHousePlacement() {
+
+    }
+    public boolean canPlaceHouse(Player player, House house) {
+        return false;
+    }
+    public ArrayList<House> housesToHighlight() {
+        return null;
+    }
 }
