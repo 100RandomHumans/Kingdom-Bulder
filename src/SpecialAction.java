08b91e2379bceca36344532987a71323d312060f
Stanley <stanleymmm2006@gmail.com>
2023-04-11 07:46:14 -0500

    commit GameLogic.java

--- src/SpecialAction.java
+++ src/SpecialAction.java
@@ -1,2 +1,11 @@
+import java.util.ArrayList;
+
 public class SpecialAction {
+    private String locationName;
+    public SpecialAction() {
+
+    }
+    public ArrayList<House> housesToHighlight(String color) {
+        return null;
+    }
 }
