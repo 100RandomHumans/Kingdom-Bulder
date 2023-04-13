ece2029b6210bf3f844a7a0a92dc727144ccae88
ananya <ananyagudapati9@gmail.com>
2023-04-13 07:47:07 -0500

    commit GameState.java

--- src/GameState.java
+++ src/GameState.java
@@ -1,7 +1,10 @@
 public class GameState {
     private int gameState;
+    private int firstTurn;
+    private int turnNum;
     private String scene;
     private AvailableHousePlacement availableHouse;
+    private ArrayList<Player> players;
     public void setScene(int state) {
         if(state = 0) {
             scene = "playScreen";
@@ -15,18 +18,36 @@ public class GameState {
     }
 
     public void startGame() {
+        GameLogic gameLogic = new GameLogic();
+        firstPlayer = gameLogic.findFirstPlayer();
+        String num = firstPlayer(firstPlayer.length() - 1);
+        turnNum = Integer.parseInt(num);
+        firstTurn = turnNum;
         gameState = 0;
         setScene(gameState);
     }
 
     public void startTurn() {
-
+        Player currentPlayer = new Player();
+        currentPlayer = players.get(turnNum - 1);
     }
 
     public void nextTurn() {
+        if(turnNum == 4) {
+            turnNum = 1;
+        } else {
+            turnNum++;
+        }
+
         availableHouse = new AvailableHousePlacement();
         if(availableHouse.canPlaceHouse() == false) {
-
+            while(turnNum != firstTurn) {
+                if(turnNum == 4) {
+                    turnNum = 1;
+                } else {
+                    turnNum++;
+                }
+            }
         }
     }
 
