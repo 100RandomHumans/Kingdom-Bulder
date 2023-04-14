69a9413de00ecc2a4d09129eb6252197b663e363
ananya <ananyagudapati9@gmail.com>
2023-04-14 08:02:05 -0500

    commit SpecialAction.java

--- src/SpecialAction.java
+++ src/SpecialAction.java
@@ -1,2 +1,23 @@
+import java.util.ArrayList;
+
 public class SpecialAction {
+    private String locationName;
+
+    public SpecialAction(String specialTile) {
+        locationName = specialTile;
+    }
+
+    public ArrayList<House> highliteHouse() {
+        ArrayList<House> specialActionHouses = new ArrayList<House>();
+
+        if(locationName.equals("Farm")) {
+
+        } else if(locationName.equals("Harbor")) {
+
+        } else if(locationName.equals("Oracle")) {
+
+        } else if(locationName.equals("Tavern")) {
+
+        }
+    }
 }
