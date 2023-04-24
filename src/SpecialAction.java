2da252b3ffe600eb8c5b702bd47d31bdff43d5be
Rocky <rockytx2013@gmail.com>
2023-04-24 07:47:50 -0500

    commit Rocky

--- src/SpecialAction.java
+++ src/SpecialAction.java
@@ -18,53 +18,53 @@ public class SpecialAction {
         Tile[][] temp = new Tile[22][32];
         int counter = 0;
 
-        for (int i = 0; i < temp.length; i++) {
-            for (int j = 0; j < temp[i].length; j++) {
-                temp[i][j] = new Tile("", "");
+        for(int i = 0; i < temp.length; i++){
+            for(int j = 0; j < temp[i].length; j++){
+                temp[i][j]  = new Tile("", "");
             }
         }
-        for (int i = 1; i < board.length; i++) {
-            for (int j = 1; j < board[0].length; j++) {
-                temp[i][j] = board[i - 1][j - 1];
+        for(int i = 1; i <= 20; i++){
+            for(int j = 1; j <= i+19; j++){
+                temp[i][j] = board[i-1][j-1];
             }
         }
 
 
         if (location.equals("Oracle")) {
-            for (int i = 1; i < 21; i++) {
-                for (int j = i; j < i + 20; j++) {
+            for (int i = 1; i <= 20; i++) {
+                for (int j = i; j <= i + 19; j++) {
                     if (temp[i][j].getHouse().getColor().equals(player.getColor())) { // find all player houses on board
-                        // if the 6 tiles around it is of the correct terrain, set that tile to true on gameboard
+                        // if the 6 tiles around it is of the correct terrain, set that tile to true on highlight
                         if (temp[i][j - 1].getTerrain().equals(terrain)) {
-                            highlight[i][j - 1] = true;
+                            highlight[i - 1][j - 2] = true;
                             counter++;
                         }
                         if (temp[i - 1][j - 1].getTerrain().equals(terrain)) {
-                            highlight[i - 1][j - 1] = true;
+                            highlight[i - 2][j - 2] = true;
                             counter++;
                         }
                         if (temp[i - 1][j].getTerrain().equals(terrain)) {
-                            highlight[i - 1][j] = true;
+                            highlight[i - 2][j - 1] = true;
                             counter++;
                         }
                         if (temp[i + 1][j].getTerrain().equals(terrain)) {
-                            highlight[i + 1][j] = true;
+                            highlight[i][j - 1] = true;
                             counter++;
                         }
                         if (temp[i][j + 1].getTerrain().equals(terrain)) {
-                            highlight[i][j + 1] = true;
+                            highlight[i - 1][j] = true;
                             counter++;
                         }
                         if (temp[i + 1][j + 1].getTerrain().equals(terrain)) {
-                            highlight[i + 1][j + 1] = true;
+                            highlight[i][j] = true;
                             counter++;
                         }
                     }
                 }
             }
-            if (counter == 0) {
-                for (int i = 0; i < 20; i++) {
-                    for (int j = i; j < i + 20; j++) {
+            if(counter == 0){
+                for (int i = 0; i <= 19; i++) {
+                    for (int j = i; j <= i+19; j++) {
                         if (board[i][j].getTerrain().equals(terrain)) {
                             highlight[i][j] = true;
                         }
@@ -72,11 +72,51 @@ public class SpecialAction {
                 }
                 return highlight;
             }
-            return highlight;
         }
 
-        else if(location.equals("Farm")){
 
+        else if(location.equals("Farm")){
+            for (int i = 1; i <= 20; i++) {
+                for (int j = i; j <= i + 19; j++) {
+                    if (temp[i][j].getHouse().getColor().equals(player.getColor())) { // find all player houses on board
+                        // if the 6 tiles around it is of the correct terrain, set that tile to true on highlight
+                        if (temp[i][j - 1].getTerrain().equals("Grass")) {
+                            highlight[i - 1][j - 2] = true;
+                            counter++;
+                        }
+                        if (temp[i - 1][j - 1].getTerrain().equals("Grass")) {
+                            highlight[i - 2][j - 2] = true;
+                            counter++;
+                        }
+                        if (temp[i - 1][j].getTerrain().equals("Grass")) {
+                            highlight[i - 2][j - 1] = true;
+                            counter++;
+                        }
+                        if (temp[i + 1][j].getTerrain().equals("Grass")) {
+                            highlight[i][j - 1] = true;
+                            counter++;
+                        }
+                        if (temp[i][j + 1].getTerrain().equals("Grass")) {
+                            highlight[i - 1][j] = true;
+                            counter++;
+                        }
+                        if (temp[i + 1][j + 1].getTerrain().equals("Grass")) {
+                            highlight[i][j] = true;
+                            counter++;
+                        }
+                    }
+                }
+            }
+            if(counter == 0){
+                for (int i = 0; i <= 19; i++) {
+                    for (int j = i; j <= i+19; j++) {
+                        if (board[i][j].getTerrain().equals("Grass")) {
+                            highlight[i][j] = true;
+                        }
+                    }
+                }
+                return highlight;
+            }
         }
 
 
