e77c44f92cacac4cac76e2c95bb47bb0261efbe9
Rocky <rockytx2013@gmail.com>
2023-05-03 10:04:29 -0500

    commit Rocky

--- src/SpecialAction.java
+++ src/SpecialAction.java
@@ -125,7 +125,7 @@ public class SpecialAction {
                     cnt++;
                 }
                 //right tile
-                if(j != 29 && !board[i][j+1].hasHouse && !board[i][j].getTerrain().equals("Mountain") && !board[i][j].getTerrain().equals("Water")){
+                if(j != 19 && !board[i][j+1].hasHouse && !board[i][j].getTerrain().equals("Mountain") && !board[i][j].getTerrain().equals("Water")){
                     highlight[i][j] = true;
                     cnt++;
                 }
@@ -135,7 +135,7 @@ public class SpecialAction {
                     cnt++;
                 }
                 //tile lower right
-                if(i != 19 && j != 29 && !board[i+1][j+1].hasHouse && !board[i][j].getTerrain().equals("Mountain") && !board[i][j].getTerrain().equals("Water")){
+                if(i != 19 && j != 19 && !board[i+1][j+1].hasHouse && !board[i][j].getTerrain().equals("Mountain") && !board[i][j].getTerrain().equals("Water")){
                     highlight[i][j] = true;
                     cnt++;
                 }
@@ -202,7 +202,7 @@ public class SpecialAction {
                 }
             }
 
-            b = 29;
+            b = 19;
             for(int a = 0; a <= 19; a++){
                 //left tile
                 if(b != 0 && board[a][b-1].hasHouse && !board[a][b].getTerrain().equals("Mountain") && !board[a][b].getTerrain().equals("Water")){
@@ -210,7 +210,7 @@ public class SpecialAction {
                     cnt++;
                 }
                 //right tile
-                if(!(b >= 29) && !board[a][b+1].hasHouse && !board[a][b].getTerrain().equals("Mountain") && !board[a][b].getTerrain().equals("Water")){
+                if(b != 29 && !board[a][b+1].hasHouse && !board[a][b].getTerrain().equals("Mountain") && !board[a][b].getTerrain().equals("Water")){
                     highlight[a][b] = true;
                     cnt++;
                 }
@@ -257,6 +257,23 @@ public class SpecialAction {
         boolean[][] highlight = new boolean[20][30];
         Tile[][] board = gameBoard.getBoard();
         Tile[][] temp = new Tile[22][32];
+        for(int i = 0; i < temp.length; i++){
+            for(int j = 0; j < temp[i].length; j++){
+                temp[i][j]  = new Tile("", "");
+            }
+        }
+        for(int i = 1; i <= 20; i++){
+            for(int j = 1; j <= 30; j++){
+                temp[i][j] = board[i-1][j-1];
+            }
+        }
+        for(int i = 0; i < highlight.length; i++){
+            for(int j = 0; j < highlight[0].length; j++){
+                if(board[i][j].hasHouse){
+                    highlight[i][j] = false;
+                }
+            }
+        }
         int counter = 0;
         for (int i = 1; i <= 20; i++) {
             //System.out.println(3);
