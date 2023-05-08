215d9262bc9fd4f467461b72b14895d42464a967
ChrisC <96661292+100RandomHumans@users.noreply.github.com>
2023-05-07 22:39:32 -0500

    Delete TerrainCard.java

--- src/TerrainCard.java
+++
@@ -1,8 +0,0 @@
-import java.awt.image.BufferedImage;
-
-public class TerrainCard {
-    private BufferedImage terrainCard, CardCanyon, CardDesert, CardFlower, CardForest;
-    public TerrainCard(BufferedImage terrain) {
-        terrainCard = terrain;
-    }
-}
